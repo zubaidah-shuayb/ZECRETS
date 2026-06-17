a1e4c0c1a0a8b122057defa4f798e64e0d76f18a
zubaidah-shuayb <zubaidahshuayb000@gmail.com>
2026-06-17 13:21:13 +0100

    fix paths

--- routes/messageRoutes.js
+++ routes/messageRoutes.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const Message = require("../models/message");
+const Message = require("../models/Message");
 
 // =========================
 // AUTH MIDDLEWARE
